86fae898497c3e7b55d900f887051fb32edf9dc8
Clientastisch <daniel.lingemann@aol.com>
2020-09-06 22:06:05 +0200

    Update RepositoryTest.java

--- MongoDialect/src/test/java/me/clientastisch/mongodb/RepositoryTest.java
+++ MongoDialect/src/test/java/me/clientastisch/mongodb/RepositoryTest.java
@@ -1,11 +1,8 @@
 package me.clientastisch.mongodb;
 
-import lombok.Getter;
 import me.clientastisch.mongodb.collection.DelegateCollection;
 import me.clientastisch.mongodb.database.DelegateDatabase;
 import org.junit.jupiter.api.Test;
-
-import java.io.Serializable;
 
 class RepositoryTest {
 
@@ -19,10 +16,10 @@ class RepositoryTest {
         DelegateCollection collection = database.createCollection("humans");
 
         collection.find("name", "Fuchs").stream().findFirst().ifPresent(var -> {
-            collection.update(var, "age", (int) var.get("age") * 2);
+            collection.update(var, "age", 4);
         });
 
-        System.out.println(collection.countDocuments());
+        collection.findAll().forEach(System.out::println);
     }
 
 }
