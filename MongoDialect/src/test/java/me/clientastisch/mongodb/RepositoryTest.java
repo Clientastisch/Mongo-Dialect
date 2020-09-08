dc75724257d2d9b676c5cb93d864b31c1cf8c601
Clientastisch <daniel.lingemann@aol.com>
2020-09-08 18:36:25 +0200

    Update RepositoryTest.java

--- MongoDialect/src/test/java/me/clientastisch/mongodb/RepositoryTest.java
+++ MongoDialect/src/test/java/me/clientastisch/mongodb/RepositoryTest.java
@@ -2,6 +2,7 @@ package me.clientastisch.mongodb;
 
 import me.clientastisch.mongodb.collection.DelegateCollection;
 import me.clientastisch.mongodb.database.DelegateDatabase;
+import me.clientastisch.mongodb.filter.Filter;
 import org.bson.Document;
 import org.junit.jupiter.api.Test;
 
@@ -19,15 +20,15 @@ class RepositoryTest {
         DelegateCollection collection = database.createCollection("humans");
 
 
-        if(collection.find("list", true).stream().count() > 0)
-            collection.insert(new Document("test", Arrays.asList("string", 2, 3.2)));
+        if(collection.find("list", true).size() == 0)
+            collection.insert(new Document("list", Arrays.asList("string", 2, 3.2)));
 
 //        collection.find("name", "Fuchs").stream().findFirst().ifPresent(document -> {
 //            collection.update(document, "age", 4);
 //        });
-
+//
 //        collection.find(Filter.EQUALS, "age", 1, 2, 3, 4).forEach(System.out::println);
-
+//
 //        collection.find("value", 1).stream().findFirst().ifPresent(document -> {
 //            collection.update(document, "list", ((Document) document.get("list")).append("var6", 6));
 //        });
