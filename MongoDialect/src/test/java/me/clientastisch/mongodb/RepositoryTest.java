0eaed5c75bee92b2cb34923ad87c37866ba9a0eb
Clientastisch <daniel.lingemann@aol.com>
2020-10-23 19:28:04 +0200

    Update RepositoryTest.java

--- MongoDialect/src/test/java/me/clientastisch/mongodb/RepositoryTest.java
+++ MongoDialect/src/test/java/me/clientastisch/mongodb/RepositoryTest.java
@@ -2,35 +2,22 @@ package me.clientastisch.mongodb;
 
 import me.clientastisch.mongodb.collection.DelegateCollection;
 import me.clientastisch.mongodb.database.DelegateDatabase;
+import me.clientastisch.mongodb.filter.Filter;
 import org.bson.Document;
 import org.junit.jupiter.api.Test;
-
-import java.util.Arrays;
 
 class RepositoryTest {
 
     @Test
     public void testRepository() {
         Repository repository = new Repository("localhost", 27017)
-                .setTimeout(10000)
                 .initialize();
 
         DelegateDatabase database = repository.getDatabase("test");
         DelegateCollection collection = database.createCollection("humans");
 
+        collection.insert(new Document("age", 1));
 
-        if(collection.find("list", true).size() == 0)
-            collection.insert(new Document("list", Arrays.asList("string", 2, 3.2)));
-
-//        collection.find("name", "Fuchs").stream().findFirst().ifPresent(document -> {
-//            collection.update(document, "age", 4);
-//        });
-//
-//        collection.find(Filter.EQUALS, "age", 1, 2, 3, 4).forEach(System.out::println);
-//
-//        collection.find("value", 1).stream().findFirst().ifPresent(document -> {
-//            collection.update(document, "list", ((Document) document.get("list")).append("var6", 6));
-//        });
+        collection.find(Filter.EQUALS, "age", 1, 2, 3, 4).forEach(System.out::println);
     }
-
 }
